72f7fc4971c272e379949af52404e9a0e6333579
Pierre Charles <pierre.charles@holidayextras.com>
2019-03-28 11:14:31 +0000

    Rendering text from API into Weather.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,14 @@ import Weather from './Weather'
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = { 
+      city: undefined,
+      country: undefined,
+      name: undefined,
+      temp: undefined,
+      desc: undefined,
+      error: undefined
+    }
   }
 
   getWeather = async (e) => {
@@ -15,16 +22,40 @@ export default class App extends React.Component {
     const country = e.target.elements.country.value
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.apiKey}&units=metric`);
     const data = await api_call.json();
-    console.log('Async function', data)
+    if (city && country){
+    this.setState(
+      { 
+        city: data.name,
+        country: data.sys.country,
+        temp: data.main.temp,
+        desc: data.weather[0].description,
+        error: undefined
+      }
+    )
+    } else {
+      this.setState({
+        city: undefined,
+        country: undefined,
+        temp: undefined,
+        desc: undefined,
+        error: 'Please enter a city and a country.'
+      })
+    }
   }
 
-  
   render() {
     return (
     <div>
       <h1>OpenWeather App</h1>
       <Form getWeather={this.getWeather}/>
-      <Weather />
+      <Weather
+        city={this.state.city}
+        country={this.state.country}
+        name={this.state.name}
+        temp={this.state.temp}
+        desc={this.state.desc}
+        error={this.state.error}
+      />
     </div>
    )
   }
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default class Weather extends React.Component {
+export default class Weather extends Component {
   render() {
-    return(
+    return (
       <div>
-        <p>Weather information goes here...</p>
+        { this.props.city && this.props.country && <p>Location: {this.props.city}</p> }
+        { this.props.temp && <p>Temperature: {this.props.temp}</p> }
+        { this.props.desc && <p>Conditions: {this.props.desc}</p> }
+        { this.props.error && <p>{this.props.error}</p> }
       </div>
     )
   }
