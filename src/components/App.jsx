8b36cf66c14af39fee02fc858cd40be55c29d5e2
Pierre Charles <pierre.charles@holidayextras.com>
2019-04-05 18:43:16 +0100

    Including button to fetch weather from current coords

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
 import Form from './Form'
 import Weather from './Weather'
 import Location from './Location'
+
+import index from '../stylesheets/Index.scss'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -13,34 +14,33 @@ export default class App extends React.Component {
       name: undefined,
       temp: undefined,
       desc: undefined,
-      error: undefined
+      error: undefined,
+      latitude: null,
+      longitude: null
     }
   }
 
-  async componentDidMount() {
-    this.getIpAddress()
-  }
-  
-  getIpAddress = async () => {
-    const apiCall = await fetch(`https://api.ipify.org/?format=json`)
-    const response = await apiCall.json()
-    const ipAddress = response.ip
-    this.setState({ ip: ipAddress })
-    console.log('Your IP address is:', ipAddress)
-    this.getGeolocation()
-  }
-  
-  getGeolocation = async () => {
-    const apiCall = await fetch(`https://geo.ipify.org/api/v1?apiKey=${process.env.geoIpifyApiKey}&ipAddress=${this.state.ip}`)
-    const response = await apiCall.json()
-    console.log('geoIpify', response.location.city, response.location.lat, response.location.lng)
-    this.setState({ lat: response.location.lat })
-    this.setState({ lng: response.location.lng })
-    this.getWeathers()
+  componentDidMount() {
+    this.getLocation()
   }
 
+  getLocation = () => {
+    const showPosition = position => {
+      console.log(position.coords.latitude)
+      console.log(position.coords.longitude)
+      this.setState({ latitude: position.coords.latitude })
+      this.setState({ longitude: position.coords.longitude })
+    }
+
+    if (navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+      console.log(`Could not get your location as Geolocation is not supported by this browser`)
+    }
+    }
+
   getWeathers = async () => {
-    const apiCall = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lng}&appid=${process.env.apiKey}&units=metric`)
+    const apiCall = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=${process.env.apiKey}&units=metric`)
     const response = await apiCall.json()
     console.log('WEATHER ======>', response)
   }
@@ -77,13 +77,9 @@ export default class App extends React.Component {
     return (
     <div>
       <h1>OpenWeather App</h1>
+      <h2>Your location is: {this.state.latitude} {this.state.longitude}</h2>
+      <button onClick={this.getWeathers}>Get weather</button>
       <Form getWeather={this.getWeather}/>
-      <Location
-        getIpAddress={this.getIpAddress}
-        getWeathers={this.getWeathers}
-        getGeolocation={this.getGeolocation}
-
-      />
       <Weather
         city={this.state.city}
         country={this.state.country}
--- src/components/Location.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Location = props => (
-  <button onClick={props.getWeathers}>Find my location</button>
-)
-
-export default Location
