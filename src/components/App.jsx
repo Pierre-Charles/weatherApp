1c18659ed3ffd3e9df925056be029d0a2efcff0e
Pierre Charles <40791587+Pierre-Charles@users.noreply.github.com>
2019-12-18 14:29:09 +0000

    Making changes and getting weather from current location

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,10 @@ import Clock from './Clock'
 import index from '../stylesheets/Index.scss'
 
 export default class App extends Component {
+
   constructor(props) {
     super(props)
-    this.state = { 
+    this.state = {
       city: undefined,
       country: undefined,
       name: undefined,
@@ -19,6 +20,7 @@ export default class App extends Component {
       isLoading: false,
       latitude: null,
       longitude: null,
+      location: undefined
     }
   }
 
@@ -27,42 +29,49 @@ export default class App extends Component {
     const showPosition = position => {
       this.setState({ latitude: position.coords.latitude })
       this.setState({ longitude: position.coords.longitude })
-      if (this.state.latitude && this.state.longitude !== undefined ) {
+      if (this.state.latitude && this.state.longitude !== undefined) {
         this.setState({ isLoading: false })
       }
-      this.getWeathers()
+      this.getWeatherFromLocation()
     }
 
-    if (navigator.geolocation){
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition)
     } else {
       console.log(`Could not get your location as Geolocation is not supported by this browser`)
     }
-    }
+  }
 
-  getWeathers = async () => {
-    const apiCall = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=${process.env.apiKey}&units=metric`)
+  getWeatherFromLocation = async () => {
+    const apiKey = '7acd2a1086122b261a2001aea0222806'
+    const apiCall = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=${apiKey}&units=metric`)
     const response = await apiCall.json()
-    console.log('WEATHER ======>', response)
+    this.setState({
+      loc: response.name,
+      ctry: response.sys.country,
+      temperature: Math.floor(response.main.temp),
+      description: response.weather[0].description
+    })
   }
 
   getWeather = async (e) => {
+    const apiKey = '7acd2a1086122b261a2001aea0222806'
     e.preventDefault()
     const city = e.target.elements.city.value
     const country = e.target.elements.country.value
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.apiKey}&units=metric`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`);
     const data = await api_call.json();
     const temperature = Math.round(data.main.temp)
-    if (city && country){
-    this.setState(
-      { 
-        city: data.name,
-        country: data.sys.country,
-        temp: temperature,
-        desc: data.weather[0].description,
-        error: undefined
-      }
-    )
+    if (city && country) {
+      this.setState(
+        {
+          city: data.name,
+          country: data.sys.country,
+          temp: temperature,
+          desc: data.weather[0].description,
+          error: undefined
+        }
+      )
     } else {
       this.setState({
         city: undefined,
@@ -76,26 +85,31 @@ export default class App extends Component {
 
   render() {
     return (
-    <Fragment>
-      <div className='main'>
-        <Clock />
-            <i onClick={this.getLocation} className="py-5 markerClick fas fa-map-marker-alt fa-2x"></i>
-          </div>
-          <Location
-            isLoading={this.state.isLoading}
-            latitude={this.state.latitude}
-            longitude={this.state.longitude}
-          />
-      <Form getWeather={this.getWeather}/>
-      <Weather
-        city={this.state.city}
-        country={this.state.country}
-        name={this.state.name}
-        temp={this.state.temp}
-        desc={this.state.desc}
-        error={this.state.error}
-      />
-    </Fragment>
-   )
+      <Fragment>
+        <div className='main'>
+          <Clock />
+          <i onClick={this.getLocation} className="py-5 markerClick fas fa-map-marker-alt fa-2x"></i>
+        </div>
+        <Location
+          isLoading={this.state.isLoading}
+          latitude={this.state.latitude}
+          longitude={this.state.longitude}
+          loc={this.state.loc}
+          ctry={this.state.ctry}
+          temperature={this.state.temperature}
+          description={this.state.description}
+        />
+        <h5 style={{ paddingTop: '100px', textAlign: 'center'}} className='mt-5'>Or check the weather in another country:</h5>
+        <Form getWeather={this.getWeather} />
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          name={this.state.name}
+          temp={this.state.temp}
+          desc={this.state.desc}
+          error={this.state.error}
+        />
+      </Fragment>
+    )
   }
 }
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -4,13 +4,14 @@ import location from '../stylesheets/Location.scss'
 
 const Location = props => (
   <Fragment>
-      { !props.latitude && !props.longitude && props.isLoading && <h3>Loading...</h3> }
-      { props.latitude && props.longitude &&
-      <div className='col-sm-12 location'>
-      <h3>Your location is:</h3>
-      <h4>{props.latitude}, {props.longitude}</h4>
+    {!props.latitude && !props.longitude && props.isLoading && <h3>Loading...</h3>}
+    {props.latitude && props.longitude &&
+      <div className='col-sm-12 location h5'>
+        <h3 className='pb-5'>Weather in {props.loc}, {props.ctry}: </h3>
+        <p>Temperature: {props.temperature}&deg;C</p>
+        <p>Condition: {props.description}</p>
       </div>
-      }
+    }
   </Fragment>
 )
 
